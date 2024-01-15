0dd3184a6c592412d5e3b8fc12e01dd41e850c70
Landon Moss <landonmoss0729@gmail.com>
2024-01-15 13:20:50 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import LoginForm from './components/LoginForm';
+import ChatFeed from './components/ChatFeed';
+
+import './App.css';
+
+const App = () => {
+    if(!localStorage.getItem('username')) return <LoginForm />
+
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectID="3bb5adff-28a6-4de8-bded-9fad3ea72110"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
+            renderChatFeed={(chatAppProps) => <ChatFeed { ...chatAppProps} />}
+        />
+    );
+}
+
+export default App;
